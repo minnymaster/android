e73eef44b152171e7f1ff1834216dace24c3a7d3
danii <dpbolotov@edu.hse.ru>
2025-06-22 15:02:02 +0500

    lj

---
+++ app/src/main/java/org/hse/finance/DB.java
@@ -0,0 +1,110 @@
+package org.hse.finance;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DB extends SQLiteOpenHelper {
+
+    public static final int DB_VERS = 1;
+    public static final String DB_NAME = "Finance";
+    public static final String T_CAT = "categories";
+    public static final String T_SPEND = "spendings";
+    public static final String CAT_ID = "_id";
+    public static final String CAT_NAME = "cat_name";
+    public static final String SPEND_ID = "_id";
+    public static final String SPEND_NAME = "spend_name";
+    public static final String SPEND_CAT = "cat_name";
+    public static final String SPEND_COST = "spend_cost";
+
+
+    public DB(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERS);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Создаем таблицу категорий
+        String createCatTable = "CREATE TABLE " + T_CAT + "(" +
+                CAT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CAT_NAME + " TEXT UNIQUE NOT NULL)";
+        db.execSQL(createCatTable);
+
+        // Создаем таблицу трат
+        String createSpendTable = "CREATE TABLE " + T_SPEND + "(" +
+                SPEND_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                SPEND_NAME + " TEXT NOT NULL," +
+                SPEND_CAT + " TEXT NOT NULL," +
+                SPEND_COST + " INTEGER NOT NULL," +
+                "FOREIGN KEY (" + SPEND_CAT + ") REFERENCES " + T_CAT + "(" + CAT_NAME + "))";
+        db.execSQL(createSpendTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + T_SPEND);
+        db.execSQL("DROP TABLE IF EXISTS " + T_CAT);
+        onCreate(db);
+    }
+
+    public Map<String, Integer> getCategorySums() {
+        Map<String, Integer> result = new HashMap<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = null;
+
+        try {
+            // Модифицированный запрос с JOIN к таблице категорий
+            String query = "SELECT " + T_CAT + "." + CAT_NAME + ", " +
+                    "SUM(" + T_SPEND + "." + SPEND_COST + ") as total " +
+                    "FROM " + T_SPEND + " " +
+                    "INNER JOIN " + T_CAT + " ON " +
+                    T_SPEND + "." + SPEND_CAT + " = " + T_CAT + "." + CAT_ID + " " +
+                    "GROUP BY " + T_CAT + "." + CAT_NAME;
+
+            cursor = db.rawQuery(query, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                int catNameIndex = cursor.getColumnIndex(CAT_NAME);
+                int totalIndex = cursor.getColumnIndex("total");
+
+                do {
+                    String categoryName = cursor.getString(catNameIndex);
+                    int total = cursor.getInt(totalIndex);
+                    result.put(categoryName, total);
+                } while (cursor.moveToNext());
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.close();
+        }
+
+        return result;
+    }
+
+    public void addTestData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Добавляем категории
+        db.execSQL("INSERT INTO " + T_CAT + "(" + CAT_NAME + ") VALUES ('Еда')");
+        db.execSQL("INSERT INTO " + T_CAT + "(" + CAT_NAME + ") VALUES ('Транспорт')");
+        db.execSQL("INSERT INTO " + T_CAT + "(" + CAT_NAME + ") VALUES ('Развлечения')");
+
+        // Добавляем траты
+        db.execSQL("INSERT INTO " + T_SPEND + "(" + SPEND_NAME + "," + SPEND_CAT + "," + SPEND_COST + ") " +
+                "VALUES ('Обед', 1, 500)");
+        db.execSQL("INSERT INTO " + T_SPEND + "(" + SPEND_NAME + "," + SPEND_CAT + "," + SPEND_COST + ") " +
+                "VALUES ('Такси', 2, 300)");
+        db.execSQL("INSERT INTO " + T_SPEND + "(" + SPEND_NAME + "," + SPEND_CAT + "," + SPEND_COST + ") " +
+                "VALUES ('Кино', 3, 700)");
+
+        db.close();
+    }
+}
--- app/src/main/java/org/hse/finance/MainActivity.java
+++ app/src/main/java/org/hse/finance/MainActivity.java
@@ -1,6 +1,8 @@
 package org.hse.finance;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,94 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DB dbHelper;
+    private PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Инициализация базы данных
+        dbHelper = new DB(this);
+
+        // Инициализация диаграммы
+        pieChart = findViewById(R.id.pieChart);
+        setupPieChart();
+        dbHelper.addTestData();
+
+        // Загрузка данных в диаграмму
+        loadChartData();
+
+        // Обработчики кнопок (если нужно)
+        ImageButton homeBtn = findViewById(R.id.imageButton);
+        ImageButton optionsBtn = findViewById(R.id.imageButton2);
+        ImageButton spendingBtn = findViewById(R.id.imageButton3);
+
+        // Добавьте обработчики кликов по кнопкам при необходимости
+    }
+
+
+
+    private void setupPieChart() {
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(android.R.color.transparent);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.animateY(1000);
+    }
+
+    private void loadChartData() {
+        // Получаем данные из БД
+        Map<String, Integer> categorySums = dbHelper.getCategorySums();
+
+        // Подготавливаем данные для диаграммы
+        List<PieEntry> entries = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : categorySums.entrySet()) {
+            entries.add(new PieEntry(entry.getValue(), entry.getKey()));
+        }
+
+        // Настраиваем набор данных
+        PieDataSet dataSet = new PieDataSet(entries, "Категории расходов");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        // Создаем объект данных и устанавливаем его в диаграмму
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+        pieChart.setData(data);
+
+        // Обновляем диаграмму
+        pieChart.invalidate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
     }
 }
